ebcaf5a1af4805c4b42f6b8c1e6c1793c7a283f9
imerrill9 <ian.merrill9@gmail.com>
2024-09-02 14:06:49 -0600

    fix image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
   Typography,
   Container,
-  Grid2 as Grid,
   Card,
   CardContent,
   Button,
@@ -29,60 +28,55 @@ const HomePage = () => {
   return (
     <>
       <Toolbar />
-      <Container sx={{ mt: 4 }}>
-        <Grid container spacing={4} justifyContent='center'>
-          <Grid item xs={12} md={8}>
-            <Card
-              sx={{
-                height: '75vh',
-                width: '70vw',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <CardMedia
-                component='img'
-                sx={{
-                  width: '70%',
-                  height: 'auto',
-                  objectFit: 'cover',
-                }}
-                image='https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTkwNTc4NDc4NDg3Nzc0MzMz/ron-swanson-from-parks-and-recreation.webp'
-                alt='Ron Swanson from Parks and Recreation'
-              />
-              <CardContent sx={{ flexGrow: 1, overflow: 'auto' }}>
-                <Typography gutterBottom variant='h5' component='div'>
-                  Ron Swanson Quote
-                </Typography>
-                <Box>
-                  {isFetching && <Typography>Loading...</Typography>}
-                  {isError && (
-                    <Typography color='error'>Error fetching quote</Typography>
-                  )}
-                  {quote && !isFetching && (
-                    <Typography
-                      variant='body1'
-                      color='text.secondary'
-                      sx={{ wordWrap: 'break-word' }}
-                    >
-                      "{quote[0]}"
-                    </Typography>
-                  )}
-                </Box>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-          <Button
-            onClick={handleGetQuote}
-            disabled={isFetching}
-            variant='contained'
+      <Container sx={{ mt: 4, display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ width: '800px' }}>
+          <Card
+            sx={{
+              height: '75vh',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
           >
-            Get Quote
-          </Button>
+            <CardMedia
+              component='img'
+              sx={{
+                objectFit: 'cover',
+              }}
+              image='https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTkwNTc4NDc4NDg3Nzc0MzMz/ron-swanson-from-parks-and-recreation.webp'
+              alt='Ron Swanson from Parks and Recreation'
+            />
+            <CardContent sx={{ flexGrow: 1, overflow: 'auto' }}>
+              <Typography gutterBottom variant='h5' component='div'>
+                Ron Swanson Quote
+              </Typography>
+              <Box>
+                {isFetching && <Typography>Loading...</Typography>}
+                {isError && (
+                  <Typography color='error'>Error fetching quote</Typography>
+                )}
+                {quote && !isFetching && (
+                  <Typography
+                    variant='body1'
+                    color='text.secondary'
+                    sx={{ wordWrap: 'break-word' }}
+                  >
+                    "{quote[0]}"
+                  </Typography>
+                )}
+              </Box>
+            </CardContent>
+          </Card>
+
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <Button
+              onClick={handleGetQuote}
+              disabled={isFetching}
+              variant='contained'
+            >
+              Get Quote
+            </Button>
+          </Box>
         </Box>
       </Container>
     </>
